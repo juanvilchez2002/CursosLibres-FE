9bf73ef7c55ff3467e253d82d4b19a105ee8a4f3
Juan A. Vilchez Huarcaya <juanvilchez2002@gmail.com>
2022-04-05 23:45:02 -0500

    mod: terminando el juego blackjack

--- JS-guia/02-blackjack/assets/js/juego.js
+++ JS-guia/02-blackjack/assets/js/juego.js
@@ -16,8 +16,13 @@ let puntosJugador = 0,
 
 // referencias del HTML
 const btnPedir = document.querySelector("#btnPedir"); // referencia al boton Pedir
+const btnDetener = document.querySelector('#btnDetener'); // referencia al boton Detener
+const btnNuevo = document.querySelector('#btnNuevo'); // referencia al boton Nuevo
+
 const puntosHTML = document.querySelectorAll('small'); 
+
 const divCartasJugador = document.querySelector('#jugador-cartas'); // referencia al div del jugador
+const divCartasComputadora = document.querySelector('#computadora-cartas'); // referencia al div de la computadora
 
 
 // función que crea una nueva baraja
@@ -108,6 +113,47 @@ const valorCarta = (carta) =>{
 const valor = valorCarta(carta);
 console.log({valor});
 
+// turno de la computadora
+const turnoComputadora = (puntosMinimos) =>{
+
+    do{
+        //pedimos una carta
+        const carta = pedirCarta();
+        
+        //sumamos puntos
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+
+        //mostramos los puntajes en el Jugador
+        puntosHTML[1].innerText = puntosComputadora;
+
+        // creamos una nueva carta
+        const imgCarta = document.createElement('img'); // creamos la imagen q estara en memoria
+        imgCarta.src = `./assets/cartas/${carta}.png`
+        imgCarta.classList.add('carta'); // añadimos la clase
+
+        divCartasComputadora.append(imgCarta);
+
+        if(puntosMinimos > 21){
+            break;
+        }
+
+    }while(( puntosComputadora < puntosMinimos ) && ( puntosMinimos <=21 ));
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+    
+} 
+
+
 // Eventos
 // elemento_html.addEventListener(Evento, función_a_ejecutar_acción)
 btnPedir.addEventListener('click', () => {
@@ -131,9 +177,48 @@ btnPedir.addEventListener('click', () => {
     // si > 21, perdio
     if(puntosJugador > 21){
         console.warn("Lo siento mucho, perdiste");
-        btnPedir.disabled = true; // desactivamos el boton de pedir más carta
+        btnPedir.disabled = true; // desactivamos el boton de pedir más carta       
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);         
+
     }else if(puntosJugador === 21) {
         console.warn("21, genial");
         btnPedir.disabled = true; // desactivamos el boton de pedir más carta
+        turnoComputadora(puntosJugador);
     }
+})
+
+// al clickear el boton detener
+btnDetener.addEventListener('click', ()=>{
+
+    // deshabilitamos el boton de pedir carta y detener
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    // la computadora realiza el pedido de la carta
+    turnoComputadora(puntosJugador);
+})
+
+// al clickear el boton nuevo;
+btnNuevo.addEventListener('click', () => {
+
+    // creamos un nuevo deck
+    deck = []
+    deck = crearDeck();
+    
+    // resetamos valores de los puntajes
+    puntosHTML[0].innerText = 0;
+    puntosHTML[1].innerText = 0;
+
+    // resetemos los puntajes
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    // resetamos las imagenes de las cartas
+    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = '';
+
+    // habilidatmos los botones
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
 })
