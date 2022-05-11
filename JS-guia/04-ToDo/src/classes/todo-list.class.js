42f637da326c158925a3a09f7c28805de3ffb4b2
Juan A. Vilchez Huarcaya <juanvilchez2002@gmail.com>
2022-05-11 01:54:18 -0500

    mod: se agrega eliminar y marcarcompletado

--- JS-guia/04-ToDo/src/classes/todo-list.class.js
+++ JS-guia/04-ToDo/src/classes/todo-list.class.js
@@ -13,11 +13,26 @@ export class TodoList{
     // eliminar una tarea a partir del Id
     eliminarTodo(id){
 
+        // usaremos filter para generar un nuevo arreglo de las tareas
+        // sin la tarea a eliminar
+        // this.TodoList.filter( todo => todo.id!=id) -> instrución para eliminar el id
+        this.todos = this.todos.filter( todo => todo.id!=id); // creamos una nueva lista
+
     }
 
     // ver si una tarea esta completada medienta Id
     marcarCompletado(id){
 
+        // realizo un recorrido de todos los componentes de las tareas
+        for(const todo of this.todos){
+            // verifico que sea la tarea seleccionada
+            // aunque uno sera un string y el otro un número
+            if(todo.id == id){
+                // cambiamos el valor que tenga a su negación
+                todo.completado = !todo.completado;
+                break; // romplemos el ciclo
+            }
+        }
     }
 
     // eliminaresmos las tareas completada
